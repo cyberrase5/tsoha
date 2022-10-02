e9598be16aba40ab3a1ddba516fdbf69187d6860
Rasmus S <rasmus.salmela@gmail.com>
2022-10-02 14:41:48 +0300

    maybe this time

--- db.py
+++ db.py
@@ -6,6 +6,6 @@ uri = getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv(uri)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
